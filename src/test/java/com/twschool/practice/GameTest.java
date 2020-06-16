deabf88ef057e0fda6fa3b06e3d2edf92f896f74
Niuyan <niuyanno.1@outlook.com>
2020-06-16 21:06:54 +0800

    5. 1A2B的情况测试通过 should_return_1A2B_given_2436

--- src/test/java/com/twschool/practice/GameTest.java
+++ src/test/java/com/twschool/practice/GameTest.java
@@ -9,9 +9,9 @@ public class GameTest {
     //    全对
     @Test
     public void should_return_4A0B_when_every_number_is_correct() {
-        String[] actualAswer = {"1", "2", "3", "4"};
+        String[] actualAnswer = {"1", "2", "3", "4"};
         String[] inputAnswer = {"1", "2", "3", "4"};
-        Game game = new Game(actualAswer);
+        Game game = new Game(actualAnswer);
 
         String result = game.guess(inputAnswer);
 
@@ -21,9 +21,9 @@ public class GameTest {
     //    全错
     @Test
     public void should_return_0A0B_when_every_number_is_wrong() {
-        String[] actualAswer = {"1", "2", "3", "4"};
+        String[] actualAnswer = {"1", "2", "3", "4"};
         String[] inputAnswer = {"5", "6", "7", "9"};
-        Game game = new Game(actualAswer);
+        Game game = new Game(actualAnswer);
 
         String result = game.guess(inputAnswer);
 
@@ -33,9 +33,9 @@ public class GameTest {
     //    有对与错
     @Test
     public void should_return_0A1B_given_2789() {
-        String[] actualAswer = {"1", "2", "3", "4"};
+        String[] actualAnswer = {"1", "2", "3", "4"};
         String[] inputAnswer = {"2", "7", "8", "9"};
-        Game game = new Game(actualAswer);
+        Game game = new Game(actualAnswer);
 
         String result = game.guess(inputAnswer);
 
@@ -45,12 +45,23 @@ public class GameTest {
 
     @Test
     public void should_return_1A0B_given_1789(){
-        String[] actualAswer = {"1", "2", "3", "4" };
+        String[] actualAnswer = {"1", "2", "3", "4" };
         String[] inputAnswer = {"1", "7", "8", "9"};
-        Game game = new Game(actualAswer);
+        Game game = new Game(actualAnswer);
 
         String result = game.guess(inputAnswer);
 
         assertEquals(result, "1A0B");
+    }
+
+    @Test
+    public void should_return_1A2B_given_2436(){
+        String[] actualAnswer = {"1", "2", "3", "4" };
+        String[] inputAnswer = {"2", "4", "3", "6"};
+        Game game = new Game(actualAnswer);
+
+        String result = game.guess(inputAnswer);
+
+        assertEquals(result, "1A2B");
     }
 }

ef2f937867e9e2defa270e020b7409c740de1465
Niuyan <niuyanno.1@outlook.com>
2020-06-16 21:02:31 +0800

    3. A0B1的情况测试通过

--- src/test/java/com/twschool/practice/GameTest.java
+++ src/test/java/com/twschool/practice/GameTest.java
@@ -6,10 +6,10 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class GameTest {
-//    全对
+    //    全对
     @Test
     public void should_return_4A0B_when_every_number_is_correct() {
-        String[] actualAswer = {"1", "2", "3", "4" };
+        String[] actualAswer = {"1", "2", "3", "4"};
         String[] inputAnswer = {"1", "2", "3", "4"};
         Game game = new Game(actualAswer);
 
@@ -18,10 +18,10 @@ public class GameTest {
         assertEquals(result, "4A0B");
     }
 
-//    全错
+    //    全错
     @Test
-    public void should_return_0A0B_when_every_number_is_wrong(){
-        String[] actualAswer = {"1", "2", "3", "4" };
+    public void should_return_0A0B_when_every_number_is_wrong() {
+        String[] actualAswer = {"1", "2", "3", "4"};
         String[] inputAnswer = {"5", "6", "7", "9"};
         Game game = new Game(actualAswer);
 
@@ -29,5 +29,16 @@ public class GameTest {
 
         assertEquals(result, "0A0B");
     }
-//    有对与错
+
+    //    有对与错
+    @Test
+    public void should_return_0A1B_when_2789() {
+        String[] actualAswer = {"1", "2", "3", "4"};
+        String[] inputAnswer = {"2", "7", "8", "9"};
+        Game game = new Game(actualAswer);
+
+        String result = game.guess(inputAnswer);
+
+        assertEquals(result, "0A1B");
+    }
 }
